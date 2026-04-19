b83760e526108c2fd01e294495871181cf1a2687
zd <yobaci@163.com>
2026-04-19 21:07:17 +0800

    feat(deck): update DecksList component logic

--- src/components/decks/DecksList.tsx
+++ src/components/decks/DecksList.tsx
@@ -2,12 +2,12 @@
 
 import { Deck } from '@/modules/decks/decks.schema'
 import { useTranslations } from 'next-intl'
-import { Breadcrumbs, SearchField } from '@heroui/react'
-import { Home } from 'lucide-react'
-import { useMemo, useState } from 'react'
-import { DecksSortDropdown } from '@/components/decks/DecksSortDropdown'
-import { DecksCardLayoutMode, DecksCardLayoutModeToggle } from '@/components/decks/DecksLayoutModeToggle'
+import { Breadcrumbs } from '@heroui/react'
+import { Home, Info, Plus } from 'lucide-react'
+import { useMemo } from 'react'
 import DecksCard from '@/components/decks/DecksCard'
+import { AppButtonLink } from '@/components/app/AppButtonLink'
+import AppLink from '@/components/app/AppLink'
 
 export default function DecksList({
   data,
@@ -21,72 +21,79 @@ export default function DecksList({
   }, [data])
 
 
-  const [mode, setMode] = useState<DecksCardLayoutMode>('grid')
-
-  const sortOptions = [
-    { label: t('decks.created-at'), key: 'created_at' },
-    { label: t('decks.updated-at'), key: 'updated_at' },
-  ]
-
-  const [sort, setSort] = useState(sortOptions[0].key)
-
   return (
-    <div className="py-6 max-w-content w-full px-4 md:px-6 mx-auto">
-      {/* Breadcrumbs */}
-      <div className="mb-6">
-        <Breadcrumbs>
-          <Breadcrumbs.Item href="/">
-            <Home className="size-4 inline mr-1 align-middle" />
-            {t('nav.home')}
-          </Breadcrumbs.Item>
-          <Breadcrumbs.Item>
-            {t('nav.decks')}
-          </Breadcrumbs.Item>
-        </Breadcrumbs>
-      </div>
 
-      {/* Header row */}
-      <div className="flex items-center justify-between mb-6 flex-wrap gap-4">
-        {/* Left side: Title and count */}
-        <div className="flex items-center gap-2">
-          <span className="text-foreground text-base font-semibold">
-            {t('decks.all-decks')}
-          </span>
-          <span className="px-2 py-0.5 rounded-full bg-default/50 text-xs font-medium">
-            {data.length}
-          </span>
-          {totalCards > 0 && (
-            <span className="text-sm text-muted">
-              ({totalCards} cards)
-            </span>
-          )}
+    <div className="py-4 max-w-content mx-auto px-3.5 min-h-[calc(100vh-19.375rem)] md:min-h-[calc(100vh-16.625rem)]">
+      <div className=" max-w-content w-full md:px-6 mx-auto">
+        {/* Breadcrumbs */}
+        <div className="mb-6">
+          <Breadcrumbs>
+            <Breadcrumbs.Item href="/">
+              <Home className="size-4 inline mr-1 align-middle" />
+              {t('nav.home')}
+            </Breadcrumbs.Item>
+            <Breadcrumbs.Item>
+              {t('nav.decks')}
+            </Breadcrumbs.Item>
+          </Breadcrumbs>
         </div>
 
-        {/* Right side: Sort, layout, search */}
-        <div className="flex items-center gap-2">
-          <DecksSortDropdown items={sortOptions} sort={sort} onSetSort={setSort} />
+        {/* Header row */}
+        <div className="flex items-center justify-between mb-2 flex-wrap gap-4 pl-2">
+          {/* Left side: Title and count */}
+          <div className="flex items-center gap-2">
+            <div className="flex items-center gap-1">
+              <span className="text-foreground text-base font-semibold">
+                {t('decks.all-decks')}
+              </span>
 
-          <DecksCardLayoutModeToggle mode={mode} setMode={setMode} />
+              <AppLink className="hover:text-accent" href={'/guide/getting-started/decks'} title="What`s Deck?">
+                <sup>
+                  <Info className="size-3" />
+                </sup>
+              </AppLink>
+            </div>
+            <span className="px-2 py-0.5 rounded-full bg-default/50 text-xs font-medium">
+              {data.length}
+            </span>
+            {totalCards > 0 && (
+              <span className="text-sm text-muted">
+                ({totalCards} cards)
+              </span>
+            )}
+          </div>
 
-          {/* Search field */}
-          <SearchField>
+          {/* Right side: Sort, layout, search */}
+          <div className="flex items-center gap-2">
+
+            <AppButtonLink href="/decks/create">
+              <Plus />
+              {t('decks.create-deck')}
+            </AppButtonLink>
+            {/* <DecksSortDropdown items={sortOptions} sort={sort} onSetSort={setSort} />
+
+          <DecksCardLayoutModeToggle mode={mode} setMode={setMode} /> */}
+
+            {/* Search field */}
+            {/* <SearchField>
             <SearchField.Group>
               <SearchField.Input
                 placeholder={t('search.placeholder')}
                 className="w-50"
               />
             </SearchField.Group>
-          </SearchField>
+          </SearchField> */}
+          </div>
         </div>
-      </div>
 
-      {/* Content placeholder */}
-      <div className="rounded-xl border-2 border-dashed border-default/30 p-12 text-center">
-        {
-          data.map((item)=>{
-            return <DecksCard key={item.id} deck={item} layoutMode={mode} />
-          })
-        }
+        {/* Content placeholder */}
+        <div className="grid gap-2">
+          {
+            data.map((item)=>{
+              return <DecksCard key={item.id} deck={item} />
+            })
+          }
+        </div>
       </div>
     </div>
   )
