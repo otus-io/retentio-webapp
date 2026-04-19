5d3fd8fc380e629e2a5e70cbbeb017508c63e2ff
zd <yobaci@163.com>
2026-04-19 21:05:52 +0800

    fix(deck): update UI styles for deck components

--- src/components/decks/DecksCard.tsx
+++ src/components/decks/DecksCard.tsx
@@ -1,32 +1,76 @@
-import { DecksCardLayoutMode } from '@/components/decks/DecksLayoutModeToggle'
+import { AppButtonLink } from '@/components/app/AppButtonLink'
+import DecksDeleteButton from '@/components/decks/DecksDeleteButton'
 import { Deck } from '@/modules/decks/decks.schema'
-import { Card, Chip } from '@heroui/react'
+import { Card, Chip, ProgressCircle } from '@heroui/react'
 
 interface DecksCardProps {
   deck: Deck,
-  layoutMode: DecksCardLayoutMode
 }
 
 export default function DecksCard({
   deck,
 }: DecksCardProps) {
   return (
-    <Card>
-      <Card.Content />
+    <Card
+      variant="default"
+      className="hover:cursor-pointer hover:shadow-lg"
+    >
       <div>
-        <Card.Header className="gap-1">
-          <Card.Title className="pr-8">{deck.name}{deck.rate}</Card.Title>
-          <Card.Description>
-            {deck.field.map((e, index)=>{
+        <Card.Header>
+          <div className="flex">
+            <Card.Title className="text-xl">
+              {deck.name}
+            </Card.Title>
+            <ProgressCircle
+              aria-label="Loading"
+              className={'ml-auto sm:ml-2 shrink-0'}
+              value={deck.rate}
+            >
+              <ProgressCircle.Track strokeWidth={6} viewBox="0 0 36 36">
+                <ProgressCircle.TrackCircle cx={18} cy={18} r={15} strokeWidth={6} />
+                <ProgressCircle.FillCircle cx={18} cy={18} r={15} strokeWidth={6} />
+              </ProgressCircle.Track>
+            </ProgressCircle>
+          </div>
+
+        </Card.Header>
+        <Card.Content className="mt-2">
+          <div className="flex gap-1 text-muted text-xs items-center font-bold ">
+            <span>{'Field'}: </span>
+            {Array.isArray(deck.field) ? deck.field.map((e, index)=>{
               return (
-                <Chip key={index}>
+                <Chip key={index} size="sm">
                   {e}
                 </Chip>
               )
-            })}
+            }) : (
+              <Chip size="sm">{deck.field}</Chip>
+            )}
+          </div>
+          <Card.Description className="flex gap-1 text-xs items-center">
+            <span>Rate: {deck.rate}</span>
+            <span>|</span>
+            <span>Cards: {deck.stats.cards_count}</span>
+            <span>|</span>
+            <span>Facts: {deck.stats.facts_count}</span>
+            <span>|</span>
+            <span>Due: {deck.stats.due_cards}</span>
           </Card.Description>
-        </Card.Header>
-        <Card.Footer />
+        </Card.Content>
+        <Card.Footer className="grid md:flex gap-2 md:justify-between">
+          <p className="flex gap-1 text-xs items-center">
+            <span>Last reviewed at: {new Date(deck.stats.last_reviewed_at * 1e3).toLocaleDateString()}</span>
+          </p>
+          <div className="flex gap-1 justify-end">
+            <AppButtonLink href={`/decks/${deck.id}`} size="sm" variant="secondary">
+              details
+            </AppButtonLink>
+            <AppButtonLink href={`/decks/${deck.id}/edit`} size="sm" variant="secondary">
+              edit
+            </AppButtonLink>
+            <DecksDeleteButton deckId={deck.id} />
+          </div>
+        </Card.Footer>
       </div>
     </Card>
   )
