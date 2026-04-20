d1e0651623abc94c7b862fdd9f2b45fe7fcd9970
zd <yobaci@163.com>
2026-04-20 23:25:04 +0800

    feat(decks): update deck edit page

--- src/app/(default)/(public)/decks/[id]/page.tsx
+++ src/app/(default)/(public)/decks/[id]/page.tsx
@@ -1,21 +1,17 @@
-import AppLink from '@/components/app/AppLink'
+'use server'
+import DecksDetail from '@/components/decks/DecksDetail'
 import { getDeckService } from '@/modules/decks/decks.service'
 import { notFound } from 'next/navigation'
 
-export default async function Page(props: PageProps<'/decks/[id]'>) {
+export default async function Page(props: PageProps<'/decks/[id]/edit'>) {
   const id = (await props.params).id
   const data = await getDeckService(id)
   if(!data.success){
     notFound()
   }
   return (
-    <div className="mx-auto max-w-content">
-      <AppLink href={`/decks/${id}/edit`}>
-        edit
-      </AppLink>
-      <pre>
-        <code>{JSON.stringify(data, null, 2)}</code>
-      </pre>
+    <div className="mx-auto max-w-content py-2 grid grid-cols-[auto_1fr]">
+      <DecksDetail deck={data.data} />
     </div>
   )
 }
