f9d11d410ad270bd7cae7fe7d478a018807525bf
zhuddan <zdecode@outlook.com>
2026-05-15 10:43:09 +0800

    feat: add AppTooltip component

---
+++ src/components/app/AppTooltip.tsx
@@ -0,0 +1,31 @@
+import { Tooltip } from '@heroui/react'
+import { ReactNode } from 'react'
+
+interface AppTooltipProps {
+  children: ReactNode
+  content: ReactNode
+  trigger?: boolean
+}
+export default function AppTooltip({
+  trigger = true,
+  children,
+  content,
+}: AppTooltipProps) {
+  return (
+    <Tooltip delay={0}>
+      {
+        trigger
+          ? (
+            <Tooltip.Trigger>
+              {children}
+            </Tooltip.Trigger>
+          )
+          :children
+      }
+
+      <Tooltip.Content>
+        {content}
+      </Tooltip.Content>
+    </Tooltip>
+  )
+}
