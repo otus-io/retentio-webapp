39192608deaee8ac7fdd5532ecd048292f30ce65
zhuddan <zdecode@outlook.com>
2026-05-29 16:19:14 +0800

    feat(tags): add tags list page with search, create and edit modal

---
+++ src/app/(default)/tags/page.tsx
@@ -0,0 +1,21 @@
+import AppError from '@/components/app/AppError'
+import { getAllTagsService } from '@/modules/tags/tags.service'
+import TagsList from '@/components/tags/TagsList'
+import { getTranslations } from 'next-intl/server'
+import type { Metadata } from 'next'
+
+export default async function Page() {
+  const { data, success, message } = await getAllTagsService()
+  if (!success) {
+    return <AppError error={message} page />
+  }
+  return <TagsList data={data?.tags || []} />
+}
+
+export async function generateMetadata() {
+  const t = await getTranslations()
+  return {
+    title: t('term.tags'),
+    description: t('common.manage', { name: t('term.tags') }),
+  } satisfies Metadata
+}
